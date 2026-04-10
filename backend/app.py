60c21ac37a750012ddaae40225ff13b9f3eb8b13
Jacob Plourde <jacob_plourde@hotmail.com>
2026-04-09 18:53:35 -0600

    Small fix with JSON files

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,7 @@ Imports and Initialization
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import bcrypt
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -19,7 +20,12 @@ CORS(app)
 Flask Routes
 """
 
-
+@app.route("/flavors", methods=["GET"])
+def get_flavors():
+    with open("flavors.json") as data:
+        flavors_json = json.load(data)
+    
+    return jsonify({}), 200
 
 """
 Startup
