3c20bd841a68ea9ab9a7ad40833cb601396fb490
Jacob Plourde <jacob_plourde@hotmail.com>
2026-04-09 19:00:13 -0600

    Get flavors backend

--- backend/app.py
+++ backend/app.py
@@ -22,10 +22,15 @@ Flask Routes
 
 @app.route("/flavors", methods=["GET"])
 def get_flavors():
-    with open("flavors.json") as data:
-        flavors_json = json.load(data)
+    try:
+        with open("flavors.json") as data:
+            flavors_json = json.load(data)
+    except FileNotFoundError:
+        return jsonify({"success": False, "message": "Flavors data file not found"}), 404
+    except json.JSONDecodeError:
+        return jsonify({"success": False, "message": "Flavors data file is invalid"}), 500
     
-    return jsonify({}), 200
+    return jsonify({"success": True, "message": "Flavors loaded.", "flavors": flavors_json}), 200
 
 """
 Startup
