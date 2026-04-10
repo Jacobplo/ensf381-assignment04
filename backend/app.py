0277d82b31c59e066a489b0a9fe9aaae82bc8a8d
Jacob Plourde <jacob_plourde@hotmail.com>
2026-04-09 18:40:25 -0600

    Initial commit

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,26 @@ Name: Jacob Plourde, UCID: 30241981
 Name: Dave Burgoin, UCID: 30214909
 """
 
+"""
+Imports and Initialization
+"""
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import bcrypt
+
+app = Flask(__name__)
+CORS(app)
+
+"""
+Flask Routes
+"""
+
+
+
+"""
+Startup
+"""
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5050)
