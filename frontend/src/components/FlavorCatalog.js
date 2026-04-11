7ea9bbe675432f3494d11502dc8bd725032af36e
Jacob Plourde <jacob_plourde@hotmail.com>
2026-04-11 15:19:47 -0600

    Flavors page WIP

--- frontend/src/components/FlavorCatalog.js
+++ frontend/src/components/FlavorCatalog.js
@@ -1,8 +1,27 @@
-import React from "react";
-import flavors from "../data/flavors";
+import React, { useState, useEffect } from "react";
 import FlavorItem from "./FlavorItem";
 
 function FlavorCatalog({ addToOrder }) {
+  const [flavors, setFlavors] = useState([]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5050/flavors", { method: "GET" })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        else {
+          throw new Error("Failed to get flavors");
+        }
+      })
+      .then(data => {
+        setMessage(data.message);
+        setFlavors(data.flavors);
+      })
+      .catch(error => setMessage(error.message))
+  })
+  
 
   return (
     <>
