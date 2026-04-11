23700dfe33f0fdfeed64c90f7e38c04549cecf20
Jacob Plourde <jacob_plourde@hotmail.com>
2026-04-11 16:19:16 -0600

    Require login for flavors page

--- frontend/src/components/FlavorCatalog.js
+++ frontend/src/components/FlavorCatalog.js
@@ -1,11 +1,20 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import FlavorItem from "./FlavorItem";
 
 function FlavorCatalog({ addToOrder }) {
   const [flavors, setFlavors] = useState([]);
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState(""); 
+  const userId = localStorage.getItem("userId");
 
-  useEffect(() => {
+  const navigate = useNavigate();
+
+  useEffect(() => {     
+    if (!userId) {
+      setMessage({ message: "You must be logged in to view order history." });
+      navigate("/login", { replace: true });
+    }
+
     fetch("http://127.0.0.1:5050/flavors", { method: "GET" })
       .then(response => {
         if (response.ok) {
@@ -20,11 +29,10 @@ function FlavorCatalog({ addToOrder }) {
         setFlavors(data.flavors);
       })
       .catch(error => setMessage(error.message))
-  })
+  }, [userId])
   
   return (
     <>
-
       <h2>Ice Cream Flavors</h2>
       <div className="flavor-grid">
 
